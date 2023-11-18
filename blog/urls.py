dfab86b18d0e59bd977a5d9e148a4ec47628d584
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-11-18 10:16:46 +0400

    done contact url part

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home,about
+from .views import home,about,contact
 
 
 
 
 urlpatterns = [
     path('', home,name='home'),
-    path('about/',about,name='about')
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact')
 ]
